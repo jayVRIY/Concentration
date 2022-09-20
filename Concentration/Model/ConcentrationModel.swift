2aa0c71a364d01f1745a318619add7faa37f96da
jayVRIY <yuanjie1224@gmail.com>
2022-09-20 23:06:38 +0800

    aaa

--- Concentration/Model/ConcentrationModel.swift
+++ Concentration/Model/ConcentrationModel.swift
@@ -8,13 +8,44 @@
 import Foundation
 class ConcentrationModel{
     var cards = [Card]()
+    var onlyOneFaceUpIndex:Int?{
+        get{
+            var foundIndex :Int?
+            for index in cards.indices{
+                if cards[index].isFaceUp {
+                    if foundIndex == nil{
+                        foundIndex = index
+                    }else{
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set{
+            for index in cards.indices{
+                cards[index].isFaceUp = (index==newValue)
+            }
+        }
+    }
     init(initialCardsNum:Int){
         for _ in 0..<initialCardsNum {
             let card = Card()
             cards += [card , card]
         }
+        cards.shuffle()
     }
     func chooseCard(at index:Int){
-        
+        if !cards[index].isMatched{
+            if let matchIndex = onlyOneFaceUpIndex, matchIndex != index{
+                if cards[matchIndex].cardID == cards[index].cardID{
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            }else{
+                onlyOneFaceUpIndex = index
+            }
+        }
     }
 }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,32 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var cardButtons: [UIButton]!
-    lazy var game = ConcentrationModel(initialCardsNum: (cardButtons.count+1)/2);
-
+    lazy var game = ConcentrationModel(initialCardsNum: pairNumOfCards);
+    var pairNumOfCards:  Int {
+        get{
+            (cardButtons.count+1)/2
+        }
+    }
+    @IBOutlet weak var flipCount: UILabel!
+    var flipTime = 0 {
+        didSet{
+            flipCount.text = "翻卡次数： \(flipTime)"
+        }
+    }
+    @IBAction func replayGame(_ sender: UIButton) {
+        game = ConcentrationModel(initialCardsNum: (cardButtons.count+1)/2);
+        emojis = ["🔪","🎡","😈","💏","🔒","❤️","🌟","🐯","🔥","🌲","🐱","👩‍🏫"];
+    }
     @IBAction func touchCard(_ sender: UIButton) {
         if let cardIndex = cardButtons.firstIndex(of:sender){
             game.chooseCard(at: cardIndex)
             updateViewFromModel()
+            flipTime+=1
         }
     }
-    let emojis = ["🔪","🎡","😈","💏"];
-    func updateViewFromModel(){
+    var emojis = ["🔪","🎡","😈","💏","🔒","❤️","🌟","🐯","🔥","🌲","🐱","👩‍🏫"];    func updateViewFromModel(){
         for index in cardButtons.indices{
             let button = cardButtons[index]
             let card = game.cards[index]
             if card.isFaceUp{
-                button.backgroundColor=UIColor.systemOrange
-                button.setTitle("", for: .normal)
-                
-            }else{
                 button.backgroundColor=UIColor.white
                 button.setTitle(emoji(for: card), for: .normal)
+            }else{
+                button.backgroundColor = card.isMatched  ? UIColor(ciColor: .clear):UIColor.systemOrange
+                button.setTitle("", for: .normal)
             }
         }
     }
+    var emojiMap:[Int:String] = [:]
     func emoji(for card:Card)->String{
-        return "?"
+        if emojiMap[card.cardID] == nil&&emojis.count>0{
+            let randomIndex = Int(arc4random_uniform (UInt32(emojis.count)))
+            emojiMap[card.cardID] = emojis.remove(at: randomIndex)
+        }
+        return emojiMap[card.cardID] ?? "?"
     }
 }
 
